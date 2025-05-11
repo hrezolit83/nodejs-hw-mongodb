89a93d0da879ea665ac942ec0b80efd007ab0088
hrezolit83 <deduh83@gmail.com>
2025-05-11 23:43:38 +0300

    hw3-crud

--- src/controlers/contacts.js
+++ src/controlers/contacts.js
@@ -1,4 +1,10 @@
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  updateContact,
+  createContact,
+  deleteContact,
+  getAllContacts,
+  getContactById,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res, next) => {
@@ -27,5 +33,44 @@ export const getContactByIdController = async (req, res, next) => {
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: student,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const contact = await deleteContact(contactId);
+
+  if (contact === null) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const patchContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
   });
 };
