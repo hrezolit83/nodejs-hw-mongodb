44cf83ee1f2dade55d3b8cdacc62370e574008ed
hrezolit83 <deduh83@gmail.com>
2025-05-11 22:19:04 +0300

    add controllers

---
+++ src/routers/contacts.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {
+  getContactsController,
+  getContactByIdController,
+} from '../controlers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+export default router;
