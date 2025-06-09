7232f95d6ba6249944cceefb7d7c8b7532497cb3
hrezolit83 <deduh83@gmail.com>
2025-06-10 00:24:16 +0300

    done

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -16,6 +17,7 @@ export const setupServer = async () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
